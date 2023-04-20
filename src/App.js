ec1feb64e16ec1494238f68dba11405f610c7d65
ManganNtengue <mangankevin28@yahoo.com>
2023-04-20 17:28:43 +0200

    🔥 F01: Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [prenom, setPrenom]=useState('')
+  const [showError, setShowError]=useState(false)
+
+  const handleChange=(e)=>{
+    e.preventDefault();
+    setPrenom(e.target.value)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(prenom===''){
+      setShowError(true)
+      
+    }else{
+      setShowError(false)
+      alert("Bonjour "+prenom)
+    }
+    
+    setPrenom('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          A Editer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>Bienvenue chez vous!</h1>
       </header>
+      <body className='App-body'>
+        <h2>
+          Connectez-vous!  
+        </h2>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" value={prenom} placeholder='Entrez votre prenom...' onChange={handleChange} />
+          {/* {showError && <label htmlFor=""> <div>Please fill in this field</div></label> } */}
+          <button type='submit'>Accedez a votre espace</button>
+        </form>
+      </body>
     </div>
   );
 }
